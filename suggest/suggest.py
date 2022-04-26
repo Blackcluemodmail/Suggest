a9298162d8b96bc1667ffc6e6f3d82dc390f7dde
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-26 13:57:10 +0530

    Update suggest.py

--- suggest/suggest.py
+++ suggest/suggest.py
@@ -121,12 +121,8 @@ class Suggest(commands.Cog):
             if not isinstance(channel, discord.TextChannel):
                 continue
             try:
-                config = await self.coll.find_one({"_id": "config"})
-                channel = self.bot.get_channel(
-                    int(config["suggestion-channel"]["channel"])
-                )
                 s_message = await channel.fetch_message(suggestion["message_id"])
-            except discord.NotFound:
+            except discord.NotFound, discord.Forbidden:
                 continue
         if not s_message:
             embed = discord.Embed(
